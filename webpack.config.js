af20eaf31db0acd52ce8f7994352349a230aca60
kumanduo <kumanduo@qq.com>
2020-06-26 13:18:04 +0800

    加载plugins-UglifyJsPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,23 @@
 const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-    entry: "./app.js",
-    output: {
-        path: path.join(__dirname, "dist"),
-        filename: "bundle.js",
-    },
-    devServer: {
-        port: 3000,
-        publicPath: "/dist",
-    },
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: [
-                "style-loader",
-                'css-loader'
-            ]
-        }]
-    },
+	entry: "./app.js",
+	output: {
+		path: path.join(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	devServer: {
+		port: 3000,
+		publicPath: "/dist",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+	plugins: [new UglifyJsPlugin()],
 };
