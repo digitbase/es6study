7d725c11618401269c540bb893b07fe11443e5c7
kumanduo <kumanduo@qq.com>
2020-06-29 23:24:56 +0800

    vue学习

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-import moduleLog from "./moduleLog";
-import "./style.css";
-
-document.write("入口 js is loadding ");
-moduleLog();
-console.log("7app.js");
-
-var [a, b, c] = [1, 2, 4];
-
-console.log(c);
+let arr = ["", , , , ,];
+console.log(0 in arr);
--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,27 @@ import ReactDom from "react-dom";
 const App = () => {
 	return (
 		<div>
-			<h1>React hao haha</h1>
+			<h1>React hao1 haha</h1>
 		</div>
 	);
 };
 
 export default App;
+
+let [a, b = "nihao"] = [1, null];
+
+console.log("a is " + a);
+console.log("b is " + b);
+
 ReactDom.render(<App />, document.getElementById("app"));
+
+let { foo, bar } = { foo: "技术胖1" };
+console.log(foo + bar);
+
+function jspang(first, ...arg) {
+	for (let val of arg) {
+		console.log(val);
+	}
+}
+
+jspang(0, 1, 2, 3, 4, 5, 6, 7);
